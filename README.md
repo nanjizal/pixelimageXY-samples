6e701cc2815a5f260a334c1518d5bde92543e725
nanjizal <nitsuj@nanjizal.net>
2023-03-12 01:51:58 +0000

    add first revised demo Arcs

--- README.md
+++ README.md
@@ -1,67 +1,10 @@
-# pixelimage-samples
-Samples to use [pixelimage](https://github.com/nanjizal/pixelimage/)
-
-## Setup
-pixelimage dependancies
-- [htmlHelper](https://github.com/nanjizal/htmlHelper/) for Html canvas tests.
-- [justPath](https://github.com/nanjizal/justPath/) for svg path data drawing.
-- [pixelimage](https://github.com/nanjizal/pixelimage/) the library.
+# pixelimageXY-samples
+Samples to use [pixelimageXY](https://github.com/nanjizal/pixelimageXY/)
   
-these not really needed but include for occasional/future use.
-- [format](https://github.com/HaxeFoundation/format/) for image conversion
-- [Vision](https://github.com/ShaharMS/Vision) for filters
+also see [TriangleGML-samples](https://github.com/nanjizal/triangleGML-samples)
   
-### These can be installed via lix. For simplicity you can do this globally.
-```
-lix install --global github:nanjizal/htmlHelper
-lix install --global github:nanjizal/justPath
-lix install --global github:nanjizal/pixelimage
-```
-```
-lix install --global github:HaxeFoundation/format
-lix install --global github:ShaharMS/Vision
-```
-### Clone this repository to test the pixelimage-samples
-```
-git clone https://github.com/nanjizal/pixelimage-samples.git
-```
+## Rewiring examples to PixelimageXY
 
-## pixelimage-canvas
-  - Arcs: [code](/src/pixelimage_samples/pixelimage_canvas/Arcs.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_arcs.html).
-  - Arrows: [code](/src/pixelimage_samples/pixelimage_canvas/Arrows.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_arrows.html).
-  - Curves: [code](/src/pixelimage_samples/pixelimage_canvas/Curves.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_curves.html).  !BROKEN!
-  - Ellipes: [code]/src/pixelimage_samples/pixelimage_canvas/Ellipses.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_ellipses.html).
-  - Lines: [code](/src/pixelimage_samples/pixelimage_canvas/Lines.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_lines.html).
-  - Quads: [code](/src/pixelimage_samples/pixelimage_canvas/Quads.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_quads.html).
-  - Rectangles: [code](/src/pixelimage_samples/pixelimage_canvas/Rectangles.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_rectangles.html).
-  - Triangles: [code](/src/pixelimage_samples/pixelimage_canvas/Triangles.hx), [html](https://nanjizal.github.io/pixelimage-samples/bin/canvas_triangles.html).
+Currently the only example setup correctly is:
   
-  
-  
-## 
-#### pixelimage-flixel
-not yet integrated
-
-#### pixelimage-openfl
-not yet integrated
-
-### pixelimage-lime
-not yet integrated
-
-### pixelimage-nme
-not yet integrated
-
-#### pixelimage-ceramic
-not yet integrated
-
-#### pixelimage-peote-view
-not yet integrated
-
-#### pixelimage-heaps
-not yet integrated
-
-#### pixelimage-kha
-not yet integrated
-
-#### pixelimage-Vision
-no samples added yet
+  - Arcs: [code](/src/pixelimageXY_samples/pixelimage_canvas/Arcs.hx), [html](https://nanjizal.github.io/pixelimageXY-samples/bin/canvas_arcs.html).
--- src/pixelimage_samples/pixelimage_canvas/Arcs.hx
+++ src/pixelimage_samples/pixelimage_canvas/Arcs.hx
@@ -2,144 +2,110 @@ package pixelimage_samples.pixelimage_canvas;
 
 import htmlHelper.canvas.CanvasSetup;
 import htmlHelper.canvas.Surface;
-import pixelimage.Pixelimage;
-import pixelimage.Pixelshape;
-import js.html.ImageElement;
-import vision.Vision;
-import vision.ds.Kernal2D;
-import pixelimage.formats.VisionImage;
-import pixelimage.triangleGML.*;
-import pixelimage.triangleGML.pattern.*;
-import pixelimage.triangleGML.coreShape.TriangleGML;
-import htmlHelper.tools.ImageLoader;
-import pixelimage.fontImage.TestLem;
-import pixelimage.fontImage.OneDfont;
-import pixelimage.textureImage.TestTiger;
-import pixelimage.textureImage.TestNineSlice;
-import pixelimage.algo.GeomPix;
+
+import pixelimageXY.Pixelshape;
+import pixelimageXY.formats.Format;
+import pixelimageXY.triangleGML.TriangleGML;
 
 function main() new Arcs();
 class Arcs {
 
     // htmlHelper requirements
     var canvasSetup = new CanvasSetup();
-    var surface :   Surface; 
-
-    var imageLoader: ImageLoader;
-
-    // main pixelimage instance
-    var pixelShape: Pixelshape;
-
     public function new(){
-        imageLoader = new ImageLoader( [], setup );
-        imageLoader.loadEncoded( [ lemBlue(), lemRed(), lemYellow(), lemCyan(), lemPurple(), lemOrange(), lemGreen() ]
-                                , [ 'lemBlue', 'lemRed', 'lemYellow', 'lemCyan', 'lemPurple', 'lemOrange', 'lemGreen' ] );
-    }
-    function setup(){
-        var images: haxe.ds.StringMap<ImageElement> = imageLoader.images;
-
-        begin();
-        drawGrid();
-        // see xml definitions below
-        TriangleGML.withString( pixelShape, arcPie, 10, 0 );
-        TriangleGML.withString( pixelShape, arcEllipse, 10, 500 );
-        TriangleGML.withString( pixelShape, arcChord, 10, 1000 );
-        TriangleGML.withString( pixelShape, arcPie2, 1210, 0 );
-        TriangleGML.withString( pixelShape, arcEllipse2, 1210, 500 );
-        TriangleGML.withString( pixelShape, arcChord2, 1210, 1000 );
-
-        var redFont = getOneDfont( images, 'lemRed' );
-        var blueFont = getOneDfont( images, 'lemBlue' );
-        var orangeFont = getOneDfont( images, 'lemOrange' );  
-        drawString( 'Arc pie', redFont, 5, 800, 300 );
-        drawString( 'Arc ellipse', blueFont, 5, 800, 300 + 500 );
-        drawString( 'Arc chord', orangeFont, 5, 800, 300 + 1000 );
-
-        end();
-    }
-
-    function getOneDfont( images: haxe.ds.StringMap<ImageElement>, fontName: String ): OneDfont {
-        var fontSrc = Pixelimage.imageElementToPixels( images.get( fontName ) );
-        return new OneDfont( fontSrc );
-    }
-
-    function drawString( str: String, font: OneDfont, scale: Int, x: Int, y: Int ){
-        var pixelImg = font.drawString( str, 0, true );
-        var scaled = pixelImg.scaleUpInt( scale, scale, true );
-        pixelShape.putPixelImage( scaled, x, y );
-    }
-
-    function begin(){
-        surface = canvasSetup.surface;
-        pixelShape = new Pixelshape( 1024*4, 768*4 );
+        var surface = canvasSetup.surface;
+        var pixelShape = new Pixelshape( 1024*4, 768*4 );
         pixelShape.transparent = true;
+        var parser = new TriangleGML( pixelShape );
+        parser.addShape( backGround, 10, 10 );
+        parser.addShape( aGrid,         10,   10 );
+        parser.addShape( arcPie,        10,    0 );
+        parser.addShape( arcEllipse,    10,  500 );
+        parser.addShape( arcChord,      10, 1000 );
+        parser.addShape( arcPie2,     1210,    0 );
+        parser.addShape( arcEllipse2, 1210,  500 );
+        parser.addShape( arcChord2,   1210, 1000 );
+        // promise used on JS to allow adding assets.
+        parser.renderPromise().then(
+            ( _ ) -> pixelShape.drawToContext( surface.me, 0, 0 )
+        );
+        
     }
 
-    function end(){
-        pixelShape.drawToContext( surface.me, 0, 0 );
-    }
-
-    function drawGrid(){
-        pixelShape.lineGrid( 100, 100, 1024*3, 768*3, 100, 2.5, 0xfF003300 );
-    }
-
+    var backGround = 
+    '<RectangleForm
+        left = "-1"
+        top = "-1"
+        width = "3073"
+        height = "2305"
+        fill = "0xFF000203">
+    </RectangleForm>';
+    
+    var aGrid = 
+    '<LineGridShape
+        left = "100"  top = "100"
+        width = "2048" height = "1536"
+        delta = "100" deltaH = "100"
+        strokeColor="0xfF003300" strokeWidth="3">
+    </LineGridShape>';
+    
     var arcPie = 
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFFFF0000" strokeWidth="12" 
         fill="0xFF00FF00" 
         startAngle="0" sweepAngle="240"
         arcType="pie">
-    </ArcShape>';
-
+    </ArcForm>';
+    
     var arcEllipse =
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFF0000FF" strokeWidth="12" 
         fill="0xFFFF00EE" 
         startAngle="0" sweepAngle="240"
         arcType="ellipse">
-    </ArcShape>';
-
+    </ArcForm>';
+    
     var arcChord =
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFFFF7700" strokeWidth="12" 
         fill="0xFF00A6FF" 
         startAngle="0" sweepAngle="240"
         arcType="chord">
-    </ArcShape>';
-
+    </ArcForm>';
+    
     var arcPie2 = 
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFFFF0000" strokeWidth="12" 
         fill="0xFF00FF00" 
         startAngle="120" sweepAngle="70"
         arcType="pie">
-    </ArcShape>';
-
+    </ArcForm>';
+    
     var arcEllipse2 =
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFF0000FF" strokeWidth="12" 
         fill="0xFFFF00EE" 
         startAngle="-43" sweepAngle="108"
         arcType="ellipse">
-    </ArcShape>';
-
+    </ArcForm>';
+    
     var arcChord2 =
-    '<ArcShape 
+    '<ArcForm 
         left="100"  top="100" 
         width="500" height="480" 
         strokeColor="0xFFFF7700" strokeWidth="12" 
         fill="0xFF00A6FF" 
         startAngle="190" sweepAngle="90"
         arcType="chord">
-    </ArcShape>';
+    </ArcForm>';
 }
